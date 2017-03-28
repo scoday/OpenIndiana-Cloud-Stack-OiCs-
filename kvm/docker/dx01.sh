5741652bec99acb530cad1eb63bacab1fbe36bd6
ScoDay <scoday@gmx.com>
2017-03-28 09:33:12 +0900

    Adding docker 01.

--- kvm/docker/dx01.sh
+++ kvm/docker/dx01.sh
@@ -3,11 +3,11 @@
         -enable-kvm \
         -vnc 0.0.0.0:1 \
         -smp 1 \
-        -m 512 \
+        -m 1024 \
         -no-hpet \
         -localtime \
-        -drive file=/dev/zvol/rdsk/rpool/kvm/sconux01/disk0,if=ide,index=0 \
-        -drive file=/home/scoday/kvm/isos/debian/deb871net.iso,media=cdrom,if=ide,index=2  \
+        -drive file=/dev/zvol/rdsk/rpool/kvm/lx01,if=ide,index=0 \
+        -drive file=/home/scoday/isos/debian/deb871net.iso,media=cdrom,if=ide,index=2  \
         -net nic,vlan=0,name=net0,model=e1000,macaddr=$MAC \
         -net vnic,vlan=0,name=net0,ifname=vnic0,macaddr=$MAC \
         -vga std
